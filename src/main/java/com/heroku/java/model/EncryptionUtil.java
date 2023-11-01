7668ce4a8a18c4b8499765cbc974a5016d1114b0
Kasper Falk Mikkelsen <kasper.falk@live.dk>
2023-11-01 18:28:37 +0100

    Added structure
    Added Controller->Service->Repository stucture

--- src/main/java/com/heroku/java/model/EncryptionUtil.java
+++ src/main/java/com/heroku/java/model/EncryptionUtil.java
@@ -4,16 +4,13 @@ import de.mkammerer.argon2.Argon2;
 import de.mkammerer.argon2.Argon2Factory;
 
 public class EncryptionUtil {
-    // Initialize Argon2 instance
     private static final Argon2 argon2 = Argon2Factory.create();
 
     public static String hashPassword(String password) {
-        // Hash the password using Argon2
         return argon2.hash(10, 65536, 1, password);
     }
 
     public static boolean verifyPassword(String hashedPassword, String inputPassword) {
-        // Verify the input password against the hashed password
         return argon2.verify(hashedPassword, inputPassword);
     }
 }
---
+++ src/main/java/com/heroku/java/service/UserService.java
@@ -0,0 +1,27 @@
+package com.heroku.java.service;
+
+import com.heroku.java.model.Constants;
+import com.heroku.java.model.EncryptionUtil;
+import com.heroku.java.model.User;
+import com.heroku.java.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+
+    public String login(String username, String password) {
+        User user = userRepository.login(username, password);
+
+        return user == null ? Constants.STATUS_FAILED : user.toJsonString();
+    }
+    public String registerUser(String username, String password, boolean isAdmin) {
+        String encryptedPassword = EncryptionUtil.hashPassword(password);
+        User user = new User(username, encryptedPassword, isAdmin);
+        return userRepository.saveUser(user);
+    }
+}
