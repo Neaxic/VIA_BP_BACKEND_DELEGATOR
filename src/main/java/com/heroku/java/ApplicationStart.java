cb3e1ce2db521e3897ac2cbca875dd51bd9d1317
Kasper Falk Mikkelsen <kasper.falk@live.dk>
2023-11-01 16:51:42 +0100

    registerUser working (Prototype)! - Hibernate working + Autowired sessionFactory
    Done alot of configuration. Consider cleanup later

--- src/main/java/com/heroku/java/ApplicationStart.java
+++ src/main/java/com/heroku/java/ApplicationStart.java
@@ -1,8 +1,15 @@
 package com.heroku.java;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -13,12 +20,16 @@ import java.util.Map;
 
 @SpringBootApplication
 @Controller
+@EntityScan("com.heroku.java.model") // Add this line
 public class ApplicationStart {
+
     private final DataSource dataSource;
+    private final SessionFactory sessionFactory;
 
     @Autowired
-    public ApplicationStart(DataSource dataSource) {
+    public ApplicationStart(DataSource dataSource, SessionFactory sessionFactory) {
         this.dataSource = dataSource;
+        this.sessionFactory = sessionFactory;
     }
 
     @GetMapping("/")
@@ -26,53 +37,8 @@ public class ApplicationStart {
         return "index";
     }
 
-
-
-    @GetMapping("/database")
-    String database(Map<String, Object> model) {
-        try (Connection connection = dataSource.getConnection()) {
-            final var statement = connection.createStatement();
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-            statement.executeUpdate("INSERT INTO ticks VALUES (now())");
-
-            final var resultSet = statement.executeQuery("SELECT tick FROM ticks");
-            final var output = new ArrayList<>();
-            while (resultSet.next()) {
-                output.add("Read from DB: " + resultSet.getTimestamp("tick"));
-            }
-
-            model.put("records", output);
-            return "database";
-
-        } catch (Throwable t) {
-            model.put("message", t.getMessage());
-            return "error";
-        }
-    }
-
-
+    // You can use sessionFactory for Hibernate operations
     public static void main(String[] args) {
         SpringApplication.run(ApplicationStart.class, args);
-/*
-        //Hibernate Setup
-        SessionFactory sessionFactory = null;
-        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-                .configure() // configures settings from hibernate.cfg.xml
-                .build();
-        try {
-            sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
-            Session session = sessionFactory.openSession();
-            User u = new User("UserName1", "Password123", true);
-            session.beginTransaction();
-            session.save(u);
-            session.getTransaction().commit();
-            session.close();
-
-        }
-        catch (Exception e) {
-            // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
-            // so destroy it manually.
-            StandardServiceRegistryBuilder.destroy( registry );
-        }*/
     }
 }
--- src/main/java/com/heroku/java/HibernateConfig.java
+++ src/main/java/com/heroku/java/HibernateConfig.java
@@ -1,27 +1,81 @@
 package com.heroku.java;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.context.annotation.Primary;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
+@EnableTransactionManagement
 public class HibernateConfig {
 
-    @Autowired
-    private DataSource dataSource;
+    private final Environment environment;
 
-    @Bean(name="entityManagerFactory")
+    public HibernateConfig(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Bean
     public LocalSessionFactoryBean sessionFactory() {
-        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
-        sessionFactoryBean.setDataSource(dataSource);
-        sessionFactoryBean.setConfigLocation(new ClassPathResource("hibernate.cfg.xml"));
-        sessionFactoryBean.setPackagesToScan("com.heroku.java.model");
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("com.heroku.java.model");
+        sessionFactory.setHibernateProperties(hibernateProperties());
 
-        return sessionFactoryBean;
+        return sessionFactory;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://ec2-99-80-190-165.eu-west-1.compute.amazonaws.com:5432/dd051b07tq1sjn");
+        dataSource.setUsername("raqavjkqhsocey");
+        dataSource.setPassword("a42dff335fe1bc8c1cd0d6278cf02130495bef60269dff076fda016f2abea13f");
+
+        return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager hibernateTransactionManager(SessionFactory sessionFactory) {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory);
+
+        return transactionManager;
+    }
+
+    private final Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+
+        return hibernateProperties;
+    }
+
+    @Bean
+    @Primary
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactory.setDataSource(dataSource());
+        entityManagerFactory.setPackagesToScan("com.heroku.java.model");
+
+        HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+        entityManagerFactory.setJpaVendorAdapter(jpaVendorAdapter);
+
+        Properties jpaProperties = hibernateProperties();
+        entityManagerFactory.setJpaProperties(jpaProperties);
+
+        return entityManagerFactory;
     }
 }
