c980655df8d3c3acb65b28a8ac508c707dd6907d
Andre <andreas79001@gmail.com>
2023-11-09 23:02:03 +0100

    Attempt to fix CORS - allowing all originpatterns

--- src/main/java/com/heroku/java/CorsConfig.java
+++ src/main/java/com/heroku/java/CorsConfig.java
@@ -8,20 +8,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                // Tillad CORS-anmodninger til alle dine API'er fra frontendens URL
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000/")
-                        .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
