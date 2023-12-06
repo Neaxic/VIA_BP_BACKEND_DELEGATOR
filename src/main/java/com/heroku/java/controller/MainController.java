ffdd9c662f022e493575e472a0059d7366593c36
Andre <andreas79001@gmail.com>
2023-12-06 18:10:27 +0100

    readded main controller deleteuser endpoint

--- src/main/java/com/heroku/java/controller/MainController.java
+++ src/main/java/com/heroku/java/controller/MainController.java
@@ -162,6 +162,11 @@ public class MainController {
         return productService.getMostFrequentStatusForMachine();
     }
 
+    @GetMapping("/deleteUser")
+    public Boolean deleteUser(@RequestParam Integer userId){
+        return userService.deleteUser(userId);
+    }
+
 
 
 }
